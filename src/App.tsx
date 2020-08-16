8980f0cd7639060d3094cdc256af074d3fa115d4
Arslan <1041238415@qq.com>
2020-08-16 23:32:13 +0800

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import Button, { ButtonSize, ButtonType } from "components/button";
+import Button, { ButtonSize, ButtonType } from "components/Button/button";
 
 function App() {
   return (
     <div className="App">
-      <Button btnType={ButtonType.Primary}>primary</Button>
+      <Button
+        btnType={ButtonType.Primary}
+        size={ButtonSize.Large}
+        className="custom"
+      >
+        primary
+      </Button>
       <Button btnType={ButtonType.Default}>default</Button>
       <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
         danger
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classnames";
+import { type } from "os";
 
 export enum ButtonSize {
   Large = "lg",
@@ -22,10 +23,26 @@ interface BaseButtonProps {
   href?: string;
 }
 
-const Button: React.FC<BaseButtonProps> = (props) => {
-  const { btnType, disabled, size, children, href } = props;
+type NativeButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>;
+
+type AnchorButtonProps = BaseButtonProps &
+  React.AnchorHTMLAttributes<HTMLElement>;
+
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    btnType,
+    disabled,
+    size,
+    children,
+    href,
+    className,
+    ...restProps
+  } = props;
   //btn,btn-lg,btn-primary
-  const classes = classNames("btn", {
+  const classes = classNames("btn", className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     disabled: btnType === ButtonType.Link && disabled,
@@ -33,13 +50,13 @@ const Button: React.FC<BaseButtonProps> = (props) => {
 
   if (btnType === ButtonType.Link && href) {
     return (
-      <a className={classes} href={href}>
+      <a className={classes} href={href} {...restProps}>
         {children}
       </a>
     );
   } else {
     return (
-      <button className={classes} disabled={disabled}>
+      <button className={classes} disabled={disabled} {...restProps}>
         {children}
       </button>
     );
